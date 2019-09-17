a837c5ef6ecc292ebf380275c8e5ff18cb40d1b0
Viktor <viktorprodeus@gmail.com>
2019-09-18 01:07:48 +0300

    refactor: вынес константы в начало кода

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Вальц', 'Онопко', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARDS_COUNTER = 4;
+
 // Нашли попап и показали его
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -11,12 +17,6 @@ var similarListElement = userDialog.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
-
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Вальц', 'Онопко', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var WIZARDS_COUNTER = 4;
 
 // Функция, возвращающая случайный элемемент массива
 function getRandomElement(array) {
