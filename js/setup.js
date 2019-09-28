4e5ffa312f155d17aa2408ed039d57a1a1891b24
Viktor <viktorprodeus@gmail.com>
2019-09-28 20:18:25 +0300

    feat: добавил обработчики событий элементам согласно ТЗ

--- js/setup.js
+++ js/setup.js
@@ -1,16 +1,101 @@
 'use strict';
 
+var ESC = 27;
+var ENTER = 13;
+
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Вальц', 'Онопко', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARDS_COUNTER = 4;
 
-// Нашли попап и показали его
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var openDialog = document.querySelector('.setup-open');
+var closeDialog = userDialog.querySelector('.setup-close');
 
-// Нашли cписок похожих персонажей
+var onEscPopupPress = function (evt) {
+  if (evt.keyCode === ESC) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onEscPopupPress);
+};
+
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+  document.removeEventListener('keydown', onEscPopupPress);
+};
+
+openDialog.addEventListener('click', function () {
+  openPopup();
+});
+
+openDialog.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER) {
+    openPopup();
+  }
+});
+
+
+closeDialog.addEventListener('click', function () {
+  closePopup();
+});
+
+closeDialog.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER) {
+    closePopup();
+  }
+});
+
+var form = document.querySelector('.setup-wizard-form');
+var userName = form.querySelector('.setup-user-name');
+var submitForm = form.querySelector('.setup-submit');
+
+userName.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER) {
+    evt.preventDefault();
+    userName.blur();
+    submitForm.focus();
+  }
+});
+
+userName.addEventListener('focus', function () {
+  document.removeEventListener('keydown', onEscPopupPress);
+});
+
+userName.addEventListener('blur', function () {
+  document.addEventListener('keydown', onEscPopupPress);
+});
+
+var wizardPlayer = document.querySelector('.setup-player');
+var fireball = document.querySelector('.setup-fireball-wrap');
+
+
+wizardPlayer.addEventListener('click', function (evt) {
+  var target = evt.target;
+
+  if (target.classList.contains('wizard-coat')) {
+    target.style.fill = getRandomElement(COAT_COLORS);
+    wizardPlayer.querySelector('input[name="coat-color"]').value = target.style.fill;
+  }
+
+  if (target.classList.contains('wizard-eyes')) {
+    target.style.fill = getRandomElement(EYES_COLORS);
+    wizardPlayer.querySelector('input[name="eyes-color"]').value = target.style.fill;
+  }
+
+  if (target.classList.contains('setup-fireball')) {
+    fireball.style.background = getRandomElement(FIREBALL_COLORS);
+    wizardPlayer.querySelector('input[name="fireball-color"]').value = fireball.style.background;
+  }
+});
+
+
+// Работа с DOM
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 
 // Нашли шаблон и контент внутри него
