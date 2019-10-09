08a0525d08293237593fa11194da85334b1dfb08
ViktorProdeus <42042470+ViktorProdeus@users.noreply.github.com>
2019-10-10 00:47:30 +0300

    change commit

--- js/backend.js
+++ js/backend.js
@@ -49,7 +49,7 @@
         onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
       });
 
-      xhr.timeout = 1; // 10s
+      xhr.timeout = TIME; // 10s
 
       xhr.open('POST', URL_POST);
       xhr.send(data);
